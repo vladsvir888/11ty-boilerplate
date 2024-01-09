b83a5f0168bf3ba09b0b7c98c5a90985c8f2c1e4
vsvirydovich <vladsvir888@gmail.com>
2024-01-09 21:10:19 +0300

    add changes in 11ty

--- 11ty/tasks/eleventy.images.js
+++ 11ty/tasks/eleventy.images.js
@@ -2,9 +2,9 @@ const path = require('node:path');
 const Image = require('@11ty/eleventy-img');
 
 module.exports = function (config) {
-  config.addTemplateFormats(['jpg', 'jpeg', 'png']);
+  config.addTemplateFormats(['jpg', 'jpeg', 'png', 'svg']);
 
-  config.addExtension(['jpg', 'jpeg', 'png'], {
+  config.addExtension(['jpg', 'jpeg', 'png', 'svg'], {
     read: false,
 
     compile: async function (inputContent, inputPath) {
@@ -12,17 +12,28 @@ module.exports = function (config) {
 
       const parsed = path.parse(inputPath);
 
-      await Image(inputPath, {
-        width: ['auto'],
-        formats: ['auto', 'webp'],
-        outputDir: parsed.dir.replace('app/_images', 'build/assets/images'),
-        filenameFormat: function (id, src, width, format, options) {
-          format = format === 'jpeg' ? 'jpg' : format;
+      if (parsed.ext !== '.svg') {
+        await Image(inputPath, {
+          width: ['auto'],
+          formats: ['auto', 'webp'],
+          outputDir: parsed.dir.replace('app/_images', 'build/assets/images'),
+          filenameFormat: function (id, src, width, format, options) {
+            format = format === 'jpeg' ? 'jpg' : format;
 
-          return `${path.parse(src).name}.${format}`;
-        },
-        statsOnly: false,
-      });
+            return `${path.parse(src).name}.${format}`;
+          },
+          statsOnly: false,
+        });
+      } else {
+        await Image(inputPath, {
+          formats: ['svg'],
+          outputDir: path.parse(inputPath).dir.replace('app/_images', 'build/assets/images'),
+          filenameFormat: function (id, src, width, format, options) {
+            return `${path.parse(src).name}.${format}`;
+          },
+          statsOnly: false,
+        });
+      }
     },
 
     compileOptions: {
--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 ## Available Scripts
 
 ```bash
-npm i                    # install dependencies
-npm run start            # run app in the development mode
-npm run build            # builds app for production
-npm run format           # run formatter
-npm run lint:css         # run stylelint
-npm run lint:js          # run eslint
+npm i                        # install dependencies
+npm run start                # run app in the development mode
+npm run build                # builds app for production
+npm run format               # run formatter
+npm run lint:style           # run stylelint
+npm run lint:script          # run eslint
 ```
